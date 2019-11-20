112530e5647bee905d47f010578e9a277a2191c5
Rishabh Nehra <rishabhnehrapersonal@gmail.com>
2019-11-20 15:36:46 +0530

    Styling for Profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Grid, Cell, Row } from '@material/react-layout-grid'
+import List, { ListItem, ListItemText } from '@material/react-list';
+
+import '@material/react-layout-grid/dist/layout-grid.css';
+import '@material/react-list/dist/list.css';
 
 class Profile extends Component {
 
@@ -40,17 +45,37 @@ class Profile extends Component {
         const { username } = this.props.match.params
         return (
             <div>
-                <h3>{user.name}</h3>
-                <p>Bio: {user.bio}</p>
-                <p>Location: {user.location}</p>
-                <p>Followers: {user.followers}</p>
-                <p>Following: {user.following}</p>
-                <p>Public Repos: {user.public_repos}</p>
-                <hr />
-                <h3>Repositories</h3>
-                <ul>
-                    {repos.map(repo => <Link to={`${username}/${repo.name}`}><li>{repo.name}</li></Link>)}
-                </ul>
+                <Grid>
+                    <Row>
+                        <Cell desktopColumns={4} tabletColumns={3} phoneColumns={2}>
+                            <img className="avatar" src={user.avatar_url} />
+                        </Cell>
+                        <Cell desktopColumns={8} tabletColumns={5} phoneColumns={2}>
+                            <h2>{user.name}</h2>
+                            <p>Location: {user.location}</p>
+                            <p>Followers: {user.followers}</p>
+                            <p>Following: {user.following}</p>
+                            <p>Public Repos: {user.public_repos}</p>
+                        </Cell>
+                    </Row>
+                    <Row>
+                        <h3>Repositories</h3>
+                        <Cell columns={12}>
+                            <List twoLine>
+                                {repos && repos.map(repo => (
+                                    <Link to={`${username}/${repo.name}`}>
+                                        <ListItem>
+                                            <ListItemText
+                                                primaryText={repo.name}
+                                                secondaryText={repo.language}
+                                            />
+                                        </ListItem>
+                                    </Link>
+                                ))}
+                            </List>
+                        </Cell>
+                    </Row>
+                </Grid>
             </div>
         )
     }
